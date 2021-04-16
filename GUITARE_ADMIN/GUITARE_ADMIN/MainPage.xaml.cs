63bc2ceefc01580134795e2ce78b0bcbc9cf8d09
julien <julien.lemoal@hotmail.fr>
2021-04-16 12:18:05 +0200

    Add project files.

---
+++ GUITARE_ADMIN/GUITARE_ADMIN/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GUITARE_ADMIN
+{
+    public partial class MainPage : ContentPage
+    {
+
+        ServiceReference1.WebService1SoapClient Le_WS;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Le_WS = new ServiceReference1.WebService1SoapClient(ServiceReference1.WebService1SoapClient.EndpointConfiguration.WebService1Soap12);
+            
+            
+
+
+            var commande = new ServiceReference1.Get_Guitare_Result();
+            var bois = new ServiceReference1.get_bois_by_id_Result();
+
+            commande = Le_WS.Get_GuitareAsync();
+
+            Etat_commande.Text = commande.Etat_Commande;
+            id_Bois.Text = (Le_WS.Get_bois_by_id(commande.idBois).FirstOrDefault().NomBois);
+            id_Bois1.Text = (Le_WS.Get_bois_by_id(commande.idBois_1).FirstOrDefault().NomBois);
+            id_Bois2.Text = (Le_WS.Get_bois_by_id(commande.idBois_2).FirstOrDefault().NomBois);
+            id_Micro.Text = (Le_WS.Get_micro_by_id(commande.idMicro.Value).FirstOrDefault().NomMicro);
+            id_Micro1.Text = (Le_WS.Get_micro_by_id(commande.idMicro_1.Value).FirstOrDefault().NomMicro);
+            id_Micro2.Text = (Le_WS.Get_micro_by_id(commande.idMicro_2).FirstOrDefault().NomMicro);
+            nomVibrato.Text = commande.nomVibrato;
+        }
+    }
+}
